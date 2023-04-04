8a7051669f644e3456315aa868af60ab60f78f1c
ZeNuW <115598030+ZeNuW@users.noreply.github.com>
2023-04-05 09:37:54 +1000

    fix: исправление CodeStyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/storage/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/storage/UserDbStorageTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller.storage;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,4 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserDbStorageTest {
+    @Test
+    public void tempMethod() {
+
+    }
 }
